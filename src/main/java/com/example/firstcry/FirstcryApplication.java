2480fce97187db0798d9c2b37884db5af2a173f4
unknown <priyankadeshpancde113@gmail.com>
2023-03-15 09:45:38 +0530

    CodePush

---
+++ src/main/java/com/example/firstcry/FirstcryApplication.java
@@ -0,0 +1,14 @@
+package com.example.firstcry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstcryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstcryApplication.class, args);
+
+	}
+
+}
